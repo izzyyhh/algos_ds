5fdb51824bed8e8ed72417b7bb58d0350a5fb815
Ismail Halili <ihalili.mmt-m2022@fh-salzburg.ac.at>
2023-06-26 18:01:54 +0200

    add potenz iteriertes quadrieren

---
+++ potenz_iter_quadrieren/Program.cs
@@ -0,0 +1,24 @@
+﻿double PotenzIteriertesQuadrieren(double baseNum, int exponent){
+    if(exponent == 1){
+        return baseNum;
+
+    } else{
+
+        if(exponent % 2 == 0){
+            double tmp = PotenzIteriertesQuadrieren(baseNum, exponent/2);
+            return tmp * tmp;
+
+        } else{
+            double tmp = PotenzIteriertesQuadrieren(baseNum, exponent/2);
+            return tmp * tmp * baseNum;
+        }
+        
+    }
+}
+
+double nine = PotenzIteriertesQuadrieren(3, 2);
+double twentyseven = PotenzIteriertesQuadrieren(3, 3);
+
+Console.WriteLine(nine == 9);
+Console.WriteLine(twentyseven == 27);
+
