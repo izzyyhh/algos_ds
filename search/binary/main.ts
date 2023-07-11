b41f74277d9b846a8579be872e0b18a1bb60deff
Ismail Halili <ihalili.mmt-b2019@fh-salzburg.ac.at>
2023-07-11 05:08:04 +0200

    add binary wip

---
+++ search/binary/main.ts
@@ -0,0 +1,21 @@
+const iterativebinarySearch = <T>(array: Array<T>, term: T) => {
+  let middle = array.length / 2;
+  let bottom = 0
+  let top = array.length;
+
+
+  while() {
+
+
+
+  }
+
+}
+
+
+const testArray = [1,2,3,3,5]
+const term = 3
+const correctIndex = 2
+const result = binarySearch(testArray, term)
+console.log(correctIndex === result)
+
