70b0dbeb5ce0d502232de4efe8c28e888afc0a51
Ismail Halili <ihalili.mmt-m2022@fh-salzburg.ac.at>
2023-07-12 20:03:16 +0200

    add iterative version

--- search/binary/main.ts
+++ search/binary/main.ts
@@ -1,15 +1,20 @@
 const iterativeBinarySearch = <T>(array: Array<T>, term: T) => {
-  let middle = array.length / 2;
   let bottom = 0
-  let top = array.length;
+  let top = array.length - 1
 
+  while( bottom <= top && top >= bottom) {
+   let middle = Math.floor( ( top + bottom ) / 2)
 
-  while() {
-
-
-
+   if(term === array[middle]) return middle
+    
+    if(term > array[middle]){
+      bottom = middle + 1
+    } else {
+      top = middle - 1
+    }
   }
 
+  return -1
 }
 
 
@@ -19,3 +24,8 @@ const correctIndex = 2
 const result = iterativeBinarySearch(testArray, term)
 console.log(correctIndex === result)
 
+const testFalsy = [1,2,3,4,5,6,7,8]
+const termFalsy = 9
+const resultFalsy = iterativeBinarySearch(testFalsy, termFalsy)
+
+console.log(resultFalsy === -1)
