78c213c05035a38ea75642ae2187bf00d16cd689
Ismail Halili <i.halili99@hotmail.com>
2023-07-14 15:22:56 +0200

    add bubble sort

--- sort/bubble/main.ts
+++ sort/bubble/main.ts
@@ -1,5 +1,15 @@
 function BubbleSort(array: Array<number>): Array<number> {
-	return []
+  for(let i = 0; i < array.length; i++) {
+    for(let j = 0; j < array.length - 1 - i; j++) {
+      if(array[j] > array[j + 1]) {
+         const tmp = array[j]
+	 array[j] = array[j + 1]
+	 array[j + 1] = tmp
+      }
+    }
+  }
+  return array
 }
 
-BubbleSort([])
+console.log(BubbleSort([1,2,4,3,2,0,1,2,8,5,3,4,5]))
+
